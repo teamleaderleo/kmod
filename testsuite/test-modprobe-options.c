e5a358b5dcd88d534f74751cdb1f7855a1d274f6
Leo <leoli_082000@hotmail.com>
2026-08-03 17:33:20 +0800

    testsuite: add recursive config control

--- testsuite/test-modprobe-options.c
+++ testsuite/test-modprobe-options.c
@@ -4,11 +4,31 @@
 
 #include "testsuite.h"
 
-static int modprobe_options_config_path(void)
+static int modprobe_options_config_path_control(void)
+{
+	return EXEC_TOOL(modprobe, "-C", "/etc/modprobe-config", "mod-loop-a");
+}
+DEFINE_TEST(modprobe_options_config_path_control,
+	.description = "check recursive modprobe preserves a config path without spaces",
+	.config = {
+		[TC_UNAME_R] = "4.4.4",
+		[TC_ROOTFS] = TESTSUITE_ROOTFS "test-modprobe/install-cmd-loop",
+	},
+	.env_vars = (const struct keyval[]) {
+		{ "MODPROBE", TOOLS_DIR "/modprobe" },
+		{ }
+	},
+	.output = {
+		.out = TESTSUITE_ROOTFS "test-modprobe/install-cmd-loop/correct-config-path.txt",
+	},
+	.modules_loaded = "",
+	);
+
+static int modprobe_options_config_path_space(void)
 {
 	return EXEC_TOOL(modprobe, "-C", "/etc/modprobe config", "mod-loop-a");
 }
-DEFINE_TEST(modprobe_options_config_path,
+DEFINE_TEST(modprobe_options_config_path_space,
 	.description = "check recursive modprobe preserves a config path containing a space",
 	.config = {
 		[TC_UNAME_R] = "4.4.4",
